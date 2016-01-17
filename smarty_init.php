d5aabef8bc961e8b0662711d3cdb161addb2b6ac
RickChem <1395314348@qq.com>
2016-01-17 19:42:39 +0800

    Init project again

---
+++ smarty_init.php
@@ -0,0 +1,15 @@
+<?php 
+include_once("smarty/Smarty.class.php"); //包含smarty类文件
+$smarty = new Smarty(); //建立smarty实例对象$smarty
+
+$smarty->caching=false; //是否使用缓存，项目在调试期间，不建议启用缓
+
+$smarty->config_dir="configs";
+$smarty->template_dir = "tpl"; //设置模板目录
+$smarty->compile_dir = "tpl-c"; //设置编译目录
+$smarty->cache_dir = "caches"; //缓存文件夹
+
+$smarty->left_delimiter = "{%";
+$smarty->right_delimiter = "%}";
+$smarty->security_settings['MODIFIER_FUNCS'] = array('truncate_cn');
+?>
---
+++ tpl-c/%%BE^BE8^BE89815B%%Main.html.php
@@ -0,0 +1,15 @@
+<?php /* Smarty version 2.6.27, created on 2016-01-17 12:34:21
+         compiled from Main.html */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+hello world!<br>
+
+<?php echo $this->_tpl_vars['test']; ?>
+
+</body>
+</html>
