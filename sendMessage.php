d4289d1ee0db3a0f334d7835765326269b98a110
daixinyan <381675152@qq.com>
2016-02-03 09:39:56 +0800

    Test file upload.

---
+++ sendMessage.php
@@ -0,0 +1,74 @@
+<?php
+<?php
+/*--------------------------------
+--------------------------------*/
+$uid = '106381';		//用户账号
+$pwd = 'qwER19960808';		//密码
+$mobile	 = '18795855867';	//号码
+$content = '信心发送测试';		//内容
+//即时发送
+$res = sendSMS($uid,$pwd,$mobile,$content);
+echo $res;
+
+//定时发送
+$time = '2010-05-27 12:11';
+$res = sendSMS($uid,$pwd,$mobile,$content,$time);
+echo $res;
+
+function sendSMS($uid,$pwd,$mobile,$content,$time='',$mid='')
+{
+    $http = 'http://api.cnsms.cn/';
+    $data = array
+    (
+        'ac'=>'send',
+        'uid'=>$uid,					//用户账号
+        'pwd'=>strtolower(md5($pwd)),	//MD5位32密码
+        'mobile'=>$mobile,				//号码
+        'content'=>$content,			//内容
+        'time'=>'2010-05-27 12:11',		//定时发送
+    );
+    $re= postSMS($http,$data);			//POST方式提交
+    if( trim($re) == '100' )
+    {
+        return "发送成功!";
+    }
+    else
+    {
+        return "发送失败!";
+    }
+}
+
+function postSMS($url,$data='')
+{
+    $row = parse_url($url);
+    $host = $row['host'];
+    $port = $row['port'] ? $row['port']:80;
+    $file = $row['path'];
+    while (list($k,$v) = each($data))
+    {
+        $post .= rawurlencode($k)."=".rawurlencode($v)."&";	//转URL标准码
+    }
+    $post = substr( $post , 0 , -1 );
+    $len = strlen($post);
+    $fp = @fsockopen( $host ,$port, $errno, $errstr, 10);
+    if (!$fp) {
+        return "$errstr ($errno)\n";
+    } else {
+        $receive = '';
+        $out = "POST $file HTTP/1.0\r\n";
+        $out .= "Host: $host\r\n";
+        $out .= "Content-type: application/x-www-form-urlencoded\r\n";
+        $out .= "Connection: Close\r\n";
+        $out .= "Content-Length: $len\r\n\r\n";
+        $out .= $post;
+        fwrite($fp, $out);
+        while (!feof($fp)) {
+            $receive .= fgets($fp, 128);
+        }
+        fclose($fp);
+        $receive = explode("\r\n\r\n",$receive);
+        unset($receive[0]);
+        return implode("",$receive);
+    }
+}
+?>
