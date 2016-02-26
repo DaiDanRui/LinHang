9133cb4d860bf356bfaeaf5f09731336e2398c2a
RickChem <1395314348@qq.com>
2016-02-26 18:29:42 +0800

    Add some html/css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 /**
  * Created by raychen on 16/2/16.
  */
+
+$(document).ready(function(){
+    var $p1 = $("#user_name").val();
+    var $p2 = $("#pwd_again").val();
+    var $p3 = $("#pwd").val();
+
+    $("#next").click(function(){
+        if (($p1=="")||($p2=="")||($p3=="")){
+            $("#next").removeAttr("href");
+        }else
+            $("#next").attr("href","Register-2.html");
+    });
+});
