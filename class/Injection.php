e6b9671e617c5da8c351c14e6c016ff89b2c32df
dxy14 <dxy14@software.nju.edu.cn>
2016-03-03 22:51:02 +0800

    Fix: now Injection can be used in $_POST and $_GET array's injection attack.

--- class/Injection.php
+++ class/Injection.php
@@ -2,11 +2,15 @@
 
 class Injection
 {
-    public static function excute($post_name) {
+    /**
+     * <strong>防注入操作：包括对$_POST与$_GET数组
+     * @param string $post_or_get_name
+     */
+    public static function excute($post_or_get_name) {
         if (!get_magic_quotes_gpc()) {
-            return addslashes($_REQUEST[$post_name]);
+            return addslashes($_REQUEST[$post_or_get_name]);
         } else {
-            return $_REQUEST[$post_name];
+            return $_REQUEST[$post_or_get_name];
         }
     }
 }
