8a8d750eedfc1e8e46ec44e519b645b4301cef2c
dxy14 <dxy14@software.nju.edu.cn>
2016-03-13 09:24:51 +0800

    Commit.

---
+++ class/Config_budget.php
@@ -0,0 +1,15 @@
+<?php
+
+class Config_budget
+{
+    
+    const budget_id = 'budget_id';
+    const transaction_id = 'transaction_id';
+    const commodity_id = 'commodity_id';
+    const pay_date = 'pay_date';
+    const payer_id = 'payer_id';
+    const holder_id = 'holder_id';
+    const table_name = 'tbl_budget';
+}
+
+?>
